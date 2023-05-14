8259061a063dfc0e44050e709361ef8cfa8153a2
unknown <Sutarimran47@gmail.com>
2023-05-14 14:46:53 +0530

    asdff

--- index.js
+++ index.js
@@ -1,117 +1,191 @@
 const express = require("express")
-const {connection} = require("./config/db")
-const {user_route} = require("./route/user.route")
-const {authenticate} = require("./middleware/auth.middleware")
-const http = require("http")
-const path = require('path');
-const redis = require("redis")
+const jwt = require("jsonwebtoken")
+const { createClient } = require("redis");
+const { connection } = require("./config/db")
+const { UserModel } = require("./model/user.model")
+const { user_route } = require("./route/user.route")
+const { authenticate } = require("./middleware/auth.middleware.js")
+const cookieParser = require('cookie-parser');
+const {BlacklistModel} = require("./model/block")
 const { Server } = require("socket.io")
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
-
-const client_id="23ca205a4e951b3d0464"
-const client_secret="a40831240ce5aeab012a248c9e5069152dcf2976"
-
-const client= redis.createClient();
-client.on('error', err => console.log('Redis Client Error', err));
-client.connect();
-
-
-
-
-const fs = require("fs")
+// const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const { dex_rout } = require("./route/dexter.routes")
+const client = createClient("redis://default:pAZIQGIYzeoDcfPm3PKrPU0gmPWpMeQo@redis-11856.c301.ap-south-1-1.ec2.cloud.redislabs.com:11856")
+client.on("error", (err) => console.log("Redis Client Error", err));
+const GitHubStrategy = require('passport-github').Strategy;
+const passport = require("passport")
+const session = require('express-session');
+const cors = require("cors")
 const app = express()
+const fs = require("fs")
+app.use(cookieParser());
+const path = require('path');
+const http = require("http")
 app.use(express.json())
- const cors = require("cors")
+client.connect();
 app.use(cors({
-  origin:"*"
+    origin: "*"
 }))
 
 
+//-------------------------------FOR GUTHUB OAUTH------------------------------------//
+app.use(session({
+    secret: "a6a74e7dc8023f676a4a9d38cf11de6bcec34933",
+    resave: false,
+    saveUninitialized: false
+}));
 
-app.use("/",user_route)
+app.use(passport.initialize());
+app.use(passport.session());
 
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
+});
 
-app.get("/", (req, res) => {
-    res.send("Welcome")
-})
+passport.deserializeUser(function (id, cb) {
+    cb(null, id);
+});
 
-app.get("/welcome",(req,res)=>{
-        res.sendFile(__dirname + "/signin.html");
-})
+passport.use(new GitHubStrategy({
+    clientID: "47cdbf9caf20df07fcd7",
+    clientSecret: "a6a74e7dc8023f676a4a9d38cf11de6bcec34933",
+    callbackURL: "http://localhost:2020/auth/github/callback"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile)
+        cb(null, profile)
+    }
+));
 
-app.get("/home", (req, res) => {
-    res.send("Home...")
-})
+app.get('/auth/github', passport.authenticate('github', { scope: ["profile", "email"] }));
 
-app.get("/room",async(req,res)=>{
-  const{code}=req.query
-  const accesstoken=await fetch("https://github.com/login/oauth/access_token",{
-    method:"POST",
-    headers:{
-    Accept: "application/json",
-    "content-type":"application/json"
-    },
-    body: JSON.stringify({
-    client_id:client_id,
-    client_secret:client_secret,
-    code
-    })
-  }).then((res)=>res.json())
-  const access_token=accesstoken.access_token
-//   console.log(code);
-  // connect room  here
+app.get('/auth/github/callback',
+    passport.authenticate('github', { failureRedirect: '/login' }),
+    function (req, res) {
+        const token = jwt.sign({ userId: "imran" }, 'imran', { expiresIn: '1h' });
+        client.set("token", token)
+        console.log(token)
+        // Successful authentication, redirect home.
+        res.redirect("http://127.0.0.1:5500/Frontend/dexter(single).html");
+    });
 
-    const userdetails=await fetch("https://api.github.com/user",{
-        headers:{
-            Authorization: `Bearer ${access_token}`,
-        }
-    }).then((res)=>res.json())
-    
-
-    // console.log(userdetails.email);
-    console.log(userdetails.name);
-    fs.writeFileSync("name.txt",userdetails.name)
-
-    const userEmail = await client.SET("userEmail",`${userdetails.email}`)
-    console.log(userEmail);
-   
-   
-    res.sendFile(path.join(__dirname, "../dexterlab.html"));
-})
-
-app.get("/rooms",(req,res)=>{
-     res.sendFile(path.join(__dirname, "../dexterlab.html"));
-})
+//---------------------------GITHUB OAUTH COMPLETED----------------------------------//
 
 
-app.use(authenticate)
 
-app.get("/dexterlab",(req,res)=>{
-    res.sendFile(path.join(__dirname, "../dexterlab.html"));
-})
-
-
-app.get("/logout", async(req, res) => {
-   const token = await client.GET("token")
-    //redis
-    if(!token){
-        res.send("token expired login again")
-    }else{
-        await client.SADD("logout", token)
-        //file
-            // const blacklisted_data = JSON.parse(fs.readFileSync("./blacklist.json","utf-8"));
-            // blacklisted_data.push(token)
-            // fs.writeFileSync("./blacklist.json", JSON.stringify(blacklisted_data))
-        // console.log(blacklisted_data);
-        res.send("Logout successfully");
+//--------------------------TO GET ALL USERS FROM DATABASE-----------------------------//
+app.get("/allUsers", async (req, res) => {
+    try {
+        const data = await UserModel.find({})
+        res.send(data)
+    } catch (error) {
+        res.send(error)
     }
 })
 
-const httpServer = http.createServer(app)
 
-httpServer.listen(2020,async ()=>{
-     try {
+//------------------------------USER ROUTE----------------------------------//
+
+app.use("/user", user_route)
+
+
+//--------------------------FROM HERE AUTHENTICATION STARTS-------------------------//
+
+app.use(authenticate)
+
+
+//------------------------------FOR LOGOUT---------------------------------------//
+app.get("/logout",async (req, res) => {
+    const token = await client.get('token')
+    const user = new BlacklistModel({token})
+    await user.save()
+    res.send({"msg":"You are logged out"})
+});
+
+//------------------------------FOR CREATING ROOM-------------------------------------//
+app.use("/room",dex_rout)
+
+
+//----------------------------WEB SOCKET------------------------------------//
+const httpServer = http.createServer(app)
+const wss = new Server(httpServer)
+let clientArr=[]
+wss.on("connection", (socket) => {
+    console.log("new client connected")
+    socket.on("join", ({ username, room }) => {
+        dis = 1;
+        let id = socket.id
+        let user = { id, username, room }
+        clientArr.push(user)
+        socket.join(user.room);
+        wss.to(user.room).emit("disc", {
+            room: user.room, users: user.username, dis: 1
+        })
+        wss.to(user.room).emit("roomUsers", {
+            room: user.room, users: getRoomUsers(user.room)
+        })
+    })
+
+    socket.on("chatMessage", (data) => {
+        socket.broadcast.emit("msgd", data)
+        socket.emit("mymsg", data)
+
+    });
+    function getRoomUsers(room) {
+        return clientArr.filter(user => user.room == room)
+    }
+
+
+    //========================till-here=============================
+
+    socket.on("html", (data) => {
+        console.log(data)
+        socket.broadcast.emit("fst", data)
+    })
+    socket.on("css", (data) => {
+        console.log(data)
+        socket.broadcast.emit("fstcss", data)
+    })
+    socket.on("js", (data) => {
+        socket.broadcast.emit("fstjs", data)
+    })
+    socket.on("write_html", ({ room_id, code }) => {
+        socket.broadcast.to(room_id).emit("write_html", { code })
+    });
+    socket.on("write_css", ({ room_id, code }) => {
+        socket.broadcast.to(room_id).emit("write_css", { code })
+    });
+    socket.on("write_js", ({ room_id, code }) => {
+        socket.broadcast.to(room_id).emit("write_js", { code })
+
+        socket.on("ad", (data) => {
+            socket.broadcast.emit("a", data)
+            socket.emit("a", data)
+        })
+    });
+    socket.on("disconnect", () => {
+        const user = userLeave(socket.id)
+        wss.to(user.room).emit("disc", {
+            room: user.room, users: user.username, dis: 0
+        })
+        wss.to(user.room).emit("roomUsers", {
+            room: user.room, users: getRoomUsers(user.room)
+        })
+    })
+    function userLeave(id) {
+        const index = clientArr.findIndex(user => user.id == id)
+        if (index !== -1) {
+            return clientArr.splice(index, 1)[0]
+        }
+    }
+})
+
+
+
+//----------------------------------LISTENING AND RUNNING SERVER-----------------------------------//
+
+httpServer.listen(2020, async () => {
+    try {
         await connection
         console.log("DB connected");
     } catch (error) {
@@ -120,81 +194,3 @@ httpServer.listen(2020,async ()=>{
     }
     console.log("Port @ localhost:2020");
 })
-
-const wss = new Server(httpServer)
-
-let clientArr = []
-let name = ""
-
-wss.on("connection", (socket) => {
-    // socket.emit("put",clientArr)
-    console.log("client connected")
-    socket.on("msg", (data) => {
-        // console.log(data)
-        socket.broadcast.emit("msgd", data)
-        socket.emit("msgd", data)
-        // console.log(data)
-    })
-
-    socket.on("join", (data) => {
-        console.log(data)
-        clientArr.push(data)
-        name = data
-        console.log(clientArr, data)
-        socket.broadcast.emit("online", clientArr)
-        socket.emit("online", clientArr)
-    })
-    
-    socket.on("html", (data) => {
-        console.log(data)
-        socket.broadcast.emit("fst", data)
-    })
-
-    socket.on("css", (data) => {
-        console.log(data)
-        socket.broadcast.emit("fstcss", data)
-    })
-
-    socket.on("js", (data) => {
-        // console.log(data)
-        socket.broadcast.emit("fstjs", data)
-        // socket.emit("pullhtml", data)
-    })
-    
-        //event for writting code
-        socket.on("write_html", ({room_id, code }) => {
-            socket.broadcast.to(room_id).emit("write_html",{code})
-    
-        });
-        socket.on("write_css", ({room_id, code }) => {
-            socket.broadcast.to(room_id).emit("write_css",{code})
-            
-        });
-        socket.on("write_js", ({room_id, code }) => {
-            socket.broadcast.to(room_id).emit("write_js",{code})
-    
-
-    socket.on("ad", (data) => {
-        // console.log(data)
-        socket.broadcast.emit("a", data)
-        socket.emit("a", data)
-    })
-    });
-
-    socket.on("sync_code", (socketId, code) => {
-        io.to(socketId).emit("code_change", { code });
-    });
-
-
-
-    socket.on("dis", async (name) => {
-        for (let i = 0; i < clientArr.length; i++) {
-            if (clientArr[i] == name) {
-                await clientArr.splice((i - 1), 1)
-            }
-        }
-        socket.broadcast.emit("disc", clientArr)
-        socket.emit("disc", clientArr)
-    })
-})
-
